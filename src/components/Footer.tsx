fcbbf5fd20476e7d78202ae5e6a2e7f46151eb1f
Akash Mukherjee <akash3.11.2000@gmail.com>
2023-11-05 22:36:10 +0530

    footer link orders are changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import LinkButton from "./LinkButton";
 import { about } from "../assets/portfolio";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
     const { socials } = about;
@@ -22,8 +23,10 @@ export default function Footer() {
         return () => clearInterval(interval);
     }, []);
     return (
-        <footer className="flex flex-col gap-y-2 md:flex-row justify-between items-center px-6 py-12">
-            <span className="local-time uppercase flex gap-4 text-base">
+        <footer className="px-6 py-12 flex flex-col items-center gap-y-4">
+            <div className="w-full flex flex-col gap-y-2 md:flex-row justify-between items-center">
+
+            <span className="local-time uppercase flex gap-4 text-base order-1 md:order-none">
                 <span>{`${about.address.city}, ${about.address.countryCode}`}</span>
                 <span className="text-secondary">{localTime}</span>
             </span>
@@ -34,9 +37,15 @@ export default function Footer() {
                     </LinkButton>
                 ))}
             </span>
-            <span className="copyright text-base">
+            <span className="copyright text-base order-1 md:order-none">
                 <span>&copy; {new Date().getFullYear()} &nbsp;</span>
                 <span className="text-secondary">{about.fullName}</span>
+            </span>
+            </div>
+            <span className="uppercase text-sm">design inspired by &nbsp;
+                    <Link className="text-secondary underline normal-case" to="https://www.seyi.dev/" target="_blank">
+                        seyi.dev
+                    </Link>
             </span>
         </footer>
     );
